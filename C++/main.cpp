41c79fc2060fe44ac1f9daa75eea12798b6b76be
Abdul Rehman <abaz77694@gmail.com>
2025-06-29 01:13:44 +0500

    Multi_Folders;

---
+++ C++/main.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+
+int main(){
+    std::cout<<"Help Me"<<std::endl;
+
+    return 0;
+}
---
+++ Extra_Cpp/sec.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+
+int main(){
+ std::cout<<"Top"<<std::endl;   
+
+ return 0;
+}
