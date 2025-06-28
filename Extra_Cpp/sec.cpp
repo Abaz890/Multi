9fd4cd16e71a0fd91a1588ed0a101bcd20c8ae01
Abdul Raheem <abdulraheemgenius23@gmail.com>
2025-06-29 01:32:40 +0500

    alert

--- Extra_Cpp/sec.cpp
+++ Extra_Cpp/sec.cpp
@@ -2,6 +2,13 @@
 
 int main(){
  std::cout<<"Top"<<std::endl;   
+ std::cout<<"Top!!"<<std::endl;   
+ std::cout<<"Top!!"<<std::endl;   
+ std::cout<<"somes make some power"<<std::endl;   
+ std::cout<<"Top powers"<<std::endl;   
+ std::cout<<"Top gun "<<std::endl;   
+ std::cout<<"top one "<<std::endl;   
+ std::cout<<"top guy"<<std::endl;   
 
  return 0;
 }
